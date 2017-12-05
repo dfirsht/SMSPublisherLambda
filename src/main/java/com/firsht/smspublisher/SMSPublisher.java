d627c16203bd11ecbed5dda679d320fbd43ed72f
Daniel Firsht <dfirsht@gmail.com>
2017-12-05 09:44:32 -0800

    changed to policy template and used topic instead of direct publish

--- src/main/java/com/firsht/smspublisher/SMSPublisher.java
+++ src/main/java/com/firsht/smspublisher/SMSPublisher.java
@@ -3,8 +3,11 @@ package com.firsht.smspublisher;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.CreateTopicRequest;
+import com.amazonaws.services.sns.model.CreateTopicResult;
 import com.amazonaws.services.sns.model.PublishRequest;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.sns.model.SubscribeRequest;
 
 
 public class SMSPublisher implements RequestHandler<CloudWatchEvent, Void> {
@@ -18,10 +21,19 @@ public class SMSPublisher implements RequestHandler<CloudWatchEvent, Void> {
 
 
     public Void handleRequest(final CloudWatchEvent request, Context context) {
+        CreateTopicRequest createTopicRequest = new CreateTopicRequest()
+                .withName("NumberTopic");
+        CreateTopicResult result = snsClient.createTopic(createTopicRequest);
+
+        SubscribeRequest subscribeRequest = new SubscribeRequest()
+                .withTopicArn(result.getTopicArn())
+                .withProtocol("sms")
+                .withEndpoint(PHONE_NUMBER);
+        snsClient.subscribe(subscribeRequest);
+
         PublishRequest publishRequest = new PublishRequest()
                 .withMessage(MESSAGE)
-                .withPhoneNumber(PHONE_NUMBER);
-
+                .withTopicArn(result.getTopicArn());
         snsClient.publish(publishRequest);
 
         System.out.println("Sent message " + MESSAGE + " to " + PHONE_NUMBER);
